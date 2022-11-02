bf0ae114c4164bea150fb6d68ad06ceb84ee480b
pmfoysal <pmfoysal@gmail.com>
2022-11-03 00:03:43 +0600

    Project Initialized

--- src/components/shared/logo.jsx
+++
@@ -1,9 +0,0 @@
-import { LogoContainer } from './logo.styled';
-
-export default function Logo() {
-   return (
-      <LogoContainer>
-         Vid <span>Player</span>
-      </LogoContainer>
-   );
-}
--- src/customs/contexts/storeProvider.js
+++
@@ -1,9 +0,0 @@
-import React, {createContext, useState} from 'react';
-
-export const StoreContext = createContext();
-
-export default function StoreProvider({children}) {
-   const [store, setStore] = useState({});
-
-   return <StoreContext.Provider value={{...store, setStore}}>{children}</StoreContext.Provider>;
-}
