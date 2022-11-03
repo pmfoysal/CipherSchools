4c603c589ee74eccf6eb39e151202d8d935d48dc
pmfoysal <pmfoysal@gmail.com>
2022-11-03 12:44:43 +0600

    auth APIs Middlewares Added

--- src/routes/api/v1/auth/index.js
+++ src/routes/api/v1/auth/index.js
@@ -1,8 +1,9 @@
+const request = require('@middlewares');
 const auth = require('express').Router();
 const controller = require('@controllers').auth;
 
 auth.route('/signin').post(controller.signin);
 auth.route('/signup').post(controller.signup);
-auth.route('/signout').get(controller.signout);
+auth.route('/signout').get(request.verifyToken, controller.signout);
 
 module.exports = auth;
