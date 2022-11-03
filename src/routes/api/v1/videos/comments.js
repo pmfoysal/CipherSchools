ee34bb73ffe06698b959913a073cd1482d69eedc
pmfoysal <pmfoysal@gmail.com>
2022-11-03 12:53:45 +0600

    videos APIs Middlewares Added

--- src/routes/api/v1/videos/comments.js
+++ src/routes/api/v1/videos/comments.js
@@ -1,9 +1,10 @@
+const request = require('@middlewares');
 const comments = require('express').Router();
 const controller = require('@controllers').comments;
 
-comments.route('/').get(controller.getComments).post(controller.postComments);
-comments.route('/:cId/like').post(controller.likeComment);
-comments.route('/:cId/dislike').post(controller.dislikeComment);
-comments.route('/:cId/reply').post(controller.replyComment);
+comments.route('/').get(request.verifyToken, controller.getComments).post(request.verifyToken, controller.postComments);
+comments.route('/:cId/like').post(request.verifyToken, controller.likeComment);
+comments.route('/:cId/dislike').post(request.verifyToken, controller.dislikeComment);
+comments.route('/:cId/reply').post(request.verifyToken, controller.replyComment);
 
 module.exports = comments;
--- src/routes/api/v1/videos/index.js
+++ src/routes/api/v1/videos/index.js
@@ -1,12 +1,16 @@
 const comments = require('./comments');
+const request = require('@middlewares');
 const videos = require('express').Router();
 const controller = require('@controllers').videos;
 
-videos.route('/').get(controller.getVideos).post(controller.postVideos);
-videos.route('/:vId').get(controller.getVideo);
-videos.route('/:vId/like').post(controller.likeVideo);
-videos.route('/:vId/dislike').post(controller.dislikeVideo);
-videos.route('/:vId/share').post(controller.shareVideo);
+videos
+   .route('/')
+   .get(request.verifyToken, controller.getVideos)
+   .post(request.verifyToken, request.verifyUser(['creator']), controller.postVideos);
+videos.route('/:vId').get(request.verifyToken, controller.getVideo);
+videos.route('/:vId/like').post(request.verifyToken, controller.likeVideo);
+videos.route('/:vId/dislike').post(request.verifyToken, controller.dislikeVideo);
+videos.route('/:vId/share').post(request.verifyToken, controller.shareVideo);
 videos.use('/:vId/comments', comments);
 
 module.exports = videos;
