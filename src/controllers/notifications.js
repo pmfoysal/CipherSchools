e715223634dac39429e513efee40949f1b0be037
pmfoysal <pmfoysal@gmail.com>
2022-11-03 09:55:46 +0600

    notifications Controllers Added

---
+++ src/controllers/notifications.js
@@ -0,0 +1,18 @@
+const { sendRes } = require('@utilities');
+const notifications = require('@services').notifications;
+
+exports.postNotifications = async (req, res) => {
+   sendRes(res, () => notifications.postNotifications(req.body));
+};
+
+exports.getNotifications = async (req, res) => {
+   sendRes(res, () => notifications.getNotifications(req.params));
+};
+
+exports.patchNotifications = async (req, res) => {
+   sendRes(res, () => notifications.patchNotifications(req.params.nId, req.body));
+};
+
+exports.deleteNotifications = async (req, res) => {
+   sendRes(res, () => notifications.deleteNotifications(req.params.nId));
+};
