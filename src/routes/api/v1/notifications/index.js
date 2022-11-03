9e51f50ef17e3ea3e1098119e8434a352d61acb1
pmfoysal <pmfoysal@gmail.com>
2022-11-03 21:37:02 +0600

    Some Issues Fixed

--- src/routes/api/v1/notifications/index.js
+++ src/routes/api/v1/notifications/index.js
@@ -2,10 +2,8 @@ const request = require('@middlewares');
 const controller = require('@controllers').notifications;
 const notifications = require('express').Router({ mergeParams: true });
 
-notifications
-   .route('/')
-   .get(request.verifyToken, controller.getNotifications)
-   .post(request.verifyToken, controller.postNotifications);
+notifications.route('/').get(request.verifyToken, controller.getNotifications);
+// .post(request.verifyToken, controller.postNotifications);
 notifications
    .route('/:nId')
    .patch(request.verifyToken, controller.patchNotifications)
