e429f8e0b64e2246fca3b8832f1cd2ffdec4931c
pmfoysal <pmfoysal@gmail.com>
2022-11-03 21:57:15 +0600

    Axios Interceptor Added

---
+++ src/customs/middlewares/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const api = axios.create({
+   baseURL: 'http://localhost:5000/api/v1',
+});
+
+api.interceptors.request.use(
+   function (config) {
+      if (!config?.headers?.authorization) {
+         config.headers.authorization = `Bearer ${window.localStorage.getItem('userToken')}`;
+      }
+      return config;
+   },
+   function (error) {
+      return Promise.reject(error);
+   }
+);
+
+api.interceptors.response.use(
+   function (response) {
+      console.log(response);
+      return response;
+   },
+   function (error) {
+      return Promise.reject(error);
+   }
+);
+
+export default api;
